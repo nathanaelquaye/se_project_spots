ceda9eaff90217363806115964e5ab3b3fa65a11
NATHANAEL QUAYE <nathanaelquaye3.0@gmail.com>
2025-09-10 00:31:48 -0600

    New update for my project and Luther King

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,32 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
-const newPostBtn =
-  document.querySelector(
-    ".profile__add-btn"
-  ); /*this is equivalent to using .profile__new-post-btn*/
+const newPostBtn = document.querySelector(".profile__new-post-btn");
+/*this is equivalent to using .profile__add-btn*/
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostLinkInput = newPostModal.querySelector("#card-image-input");
+const newPostDescriptionInput = newPostModal.querySelector(
+  "#post-description-input"
+);
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
+const cardImageEl = document.querySelector(".card");
+const cardCaptionEl = document.querySelector(".card__title");
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -18,9 +35,35 @@ editProfileCloseBtn.addEventListener("click", function () {
 });
 
 newPostBtn.addEventListener("click", function () {
+  newPostLinkInput.value = cardImageEl.src;
+  newPostDescriptionInput.value = cardCaptionEl.textContent;
   newPostModal.classList.add("modal_is-opened");
 });
 
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+//below is for new post modal or idk check
+const addCardFormElement = document.querySelector(
+  "#new-post-modal .modal__form"
+);
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+
+  // Log both input values to the console.
+  console.log("Image link:", newPostLinkInput.value);
+  console.log("Description:", newPostDescriptionInput.value);
+  newPostModal.classList.remove("modal_is-opened");
+  // Close the modal.
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+newPostForm.addEventListener("submit", handleAddCardSubmit);
